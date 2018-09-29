681d79dca1dd3984c2d04ea017adba7474a6696d
Cooper Green <cooper@beaglebone.localdomain>
2018-09-29 10:45:27 +0000

    Ground Station send commands over UART1
    Add to tty group and gpio
    
    sudo ntpdate pool.ntp.org
    sudo apt-get update
    sudo apt-get install build-essential python-dev python-pip python-smbus -y
    git clone git://github.com/adafruit/adafruit-beaglebone-io-python.git
    cd adafruit-beaglebone-io-python
    sudo python setup.py install
    cd ..
    sudo rm -rf adafruit-beaglebone-io-python
    
    Can Test by connecting to UART2
    
    >>> import Adafruit_BBIO.UART as UART
    >>> UART.setup("UART1")
    >>> UART.setup("UART2")
    >>> exit()
    
    UART2 // open in terminal
    /dev/ttyO2

---
+++ ground_station/ground.py
@@ -0,0 +1,21 @@
+import Adafruit_BBIO.UART as UART
+import serial
+
+UART.setup("UART1")
+ser = serial.Serial(port = "/dev/ttyO1", baudrate=9600)
+ser.close()
+ser.open()
+if ser.isOpen():
+	print "Serial is open!"
+	ser.write("Hello World!")
+	ser.write("\n\r")
+
+notDone = True
+
+while notDone:
+	cmd =raw_input(">")
+	ser.write(cmd)
+	ser.write("\n\r")
+
+ser.close()
+
